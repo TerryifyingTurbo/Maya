87f6eb4ea59f9813f4274d84a543a8bad1bbd487
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-03-24 15:53:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,13 +52,14 @@ bot.on("message", async message => {
   console.log("Bot killed");
   }
 
+  const reloading = bot.emojis.get("427234545805557771")
+
   if(command === `${prefix}reload`){
     if(message.author.id !== "297931837400023041")
     return message.channel.send("Only Master can use this command!");
     else
   
     // Reload ALL commands listed
-const reloading = bot.emojis.get("427234545805557771")
 
 const reloading = bot.emojis.find("name", "reloading")
 
