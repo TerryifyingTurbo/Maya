741243903703e9e924f49c4e26d163e491fd34ec
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-03-24 18:36:32 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ const refresh = bot.emojis.get("427234545805557771");
   if(message.author.id == "297931837400023041")
   const refresh = bot.emojis.find("name", "refresh");
 
-  await message.channel.send("**Reloading.**", `${refresh}`)
+  await message.channel.send(`**Reloading.** ${refresh}`)
     .then(message => bot.destroy());
     
     await bot.login(botsettings.token)
