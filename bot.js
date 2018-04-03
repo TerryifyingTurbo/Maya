f5511e2442997a392b4d7a98abb64bb5ce9f434b
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-04-02 22:38:28 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,11 +138,6 @@ My Latency is **${m.createdTimestamp - message.createdTimestamp}ms.** API Latenc
   if(message.content === "ayy"){
   const ayy = bot.emojis.find("name", "ayy");
   message.channel.send(`${ayy} Lmao`);
-}
-
-if(message.content.upper() == "HhaH"){
-  message.reply("HhaG*");
-}
 
 const intensefear = bot.emojis.get("406790609035329538");
 
