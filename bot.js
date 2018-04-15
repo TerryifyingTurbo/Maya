e59677388d61271fe9815bdcf76b1bbb519b1495
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-04-15 09:37:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,7 @@ if(command ===`${prefix}randuser`){
   message.guild.members.forEach(gm=>{
     pool.push(gm);
 
-    return message.channel.send(`I chose... ${u.username}` +  `${u.discriminator}`);
+    return message.channel.send(`I chose... ${user.Username}` + `${user.Discriminator}`);
   })
 }
 
