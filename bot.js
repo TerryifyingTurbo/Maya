aa62c3f11f1f502f66e89438b1cd7c33bb5b6342
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-04-16 16:28:07 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,13 +106,13 @@ bot.on("message", async message => {
     return;
   }
 
-  if(command === `${prefix}ping`) {
+  if(command === `${prefix}ping`){
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
     m.edit(`:ping_pong: ***Pong!***  x3 
-Response :satellite_orbital: **${message.createdTimestamp - message.createdTimestamp}ms.** 
-API Response :satellite_orbital: **${Math.round(bot.ping)}ms.**`)
+Response :satellite_orbital: **${m.createdTimestamp - message.createdTimestamp}ms.** 
+API Response :satellite_orbital: **${Math.round(bot.ping)}ms.**`);
   }
 
   if(command === `${prefix}leave`){
