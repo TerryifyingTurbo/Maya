809690aee984f91e5c12a80466abc1ae6fa6c95b
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-04-20 23:33:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -163,9 +163,9 @@ if(message.content === ";T" || (message.content === ";;T") || (message.content =
   message.reply( ";;T");
 }
 
-if(message.content ===`${prefix}succ`){
-  //let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  //const sayMessage = args.join(" ");   
+if(command === `${prefix}suck`){
+
+  const sayMessage = args.join(" ");
   let Ureplies = ["**loved** it", "**enjoyed** it", "**hate** it", "**want** more", "want it **from someone else** instead", "**liked** it"];
   let Treplies = ["an hour later", "a couple of minutes later", "after a really long time", "for about half an hour", "after a while"];
   let Preplies = ["the best", "the sloppiest", "the mad", "a decent", "an aight'"];
@@ -173,18 +173,9 @@ if(message.content ===`${prefix}succ`){
   let Uresult = Math.floor((Math.random() * Ureplies.length)); //for the reply options
   let Tresult = Math.floor((Math.random() * Treplies.length)); //for the time options
   let Presult = Math.floor((Math.random() * Preplies.length)); //for the power options
-  
 
-  //if(!reciever) return message.channel.send(`**${message.author.username}** was flexible enough and`+ " gave themself the **succ**. They seemed to have " + Ureplies[Uresult]);
-  //if(!args[1]) return message.channel.send("You sucking the air? Go put your mouth to good use and give something *or someone*  the succ");
-  //message.channel.send(`**${message.author.username}**` + " puts that mouth to good use and gave " + reciever +  Preplies[Presult] + " succ " +  Treplies[Tresult] + " They seemed to have " +  Ureplies[Uresult]);
-}
-
-if(command === `${prefix}suck`){
-
-  const sayMessage = args.join(" ");
   if(!sayMessage) return message.channel.send("You sucking the air? Go put your mouth to good use and give something *or someone*  the succ")
-  message.channel.send(`${message.author.username} puts that mouth to good use and gave `+ sayMessage + " the mad succ");
+  message.channel.send(`${message.author.username} puts that mouth to good use and gave `+ sayMessage + ` ${Preplies[Presult]}` + ` ${Treplies[Tresult]}` + " They seemed to have" + `${Ureplies[Uresult]}`);
 }
   
 if(command === `${prefix}8ball`){
