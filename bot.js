49bea710f2e976cf9fbd4f33dc8309acdf272dc7
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-04-25 23:07:46 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -409,7 +409,7 @@ if(command === `${prefix}dwsspacebar`){
   let embed = {
     "title": "SpaceBar",
     //"url": "https://discordapp.com",
-    "color": 0006136,
+    "color": "RANDOM",
     //"timestamp": "2011-04-21",
     "footer": {
       "icon_url": "https://image.flaticon.com/icons/png/512/189/189662.png",
