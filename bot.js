f5aeea4ebef7ed3ac92b2e8aaf3fc6672b02f060
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-04-29 00:45:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -428,7 +428,7 @@ if(command === `${prefix}dwsgtgalaxytruck`){
   let embed = {
     "title": "GT-GalaxyTruck",
     //"url": "https://discordapp.com",
-    "color": 7551373,
+    "color": 431330,
     //"timestamp": "2011-04-21",
     "footer": {
       "icon_url": "http://aukavinna.is/wp-content/uploads/2017/02/iconcar_2398746577.png",
@@ -448,42 +448,45 @@ if(command === `${prefix}dwsgtgalaxytruck`){
     "fields": [
       {
         "name": "Top Speed",
-        "value": "*Mach 1/767mph/1234kph*"
+        "value": "*Mach 1/767mph/1234kph*",
+        "inline": true
       },
       {
         "name": "0-60",
-        "value": "*1.7 seconds*"
+        "value": "*1.7 seconds*",
+        "inline": true
       },
       {
         "name": "MPG",
-        "value": "*3*"
+        "value": "*3*",
+        "inline": true
       },
       {
         "name": "Engine",
-        "value": "*9.7L V8 Duramax*"
+        "value": "*9.7L V8 Duramax*",
+        "inline": true
       },
       {
         "name": "Curb Weight",
-        "value": "*3300lbs. [1497kg]*" 
+        "value": "*3300lbs. [1497kg]*",
+        "inline": true 
       },
       {
         "name": "Layout",
-        "value": "*AWD, Front Engine*"
+        "value": "*AWD, Front Engine*",
+        "inline": true
       },
       {
         "name": "Overview",
         "value": "*A signature vehicle that has a custom galaxy wrap (hence the name) It is the only vehicle that is used to gain access to the SpaceBar, which is located on the moon. Though it is registered under the names of Travis Rilynar and Jesse Green, it is unknown when exactly the Galaxy Truck was manufactured. The five-seater truck has been modified to sustain the extreme atmospheric pressure, speed, and temperature*",
-        "inline": true
       },
       {
         "name": "Appearance",
         "value": "*Equipped with military-grade, high-end materials: Bullet-resistant glass, reinforced carbon fiber plating, and a hardened drivetrain. This off-road, five-seater also has an elegant wrap that glows. The interior is custom built: Motorsport racetrack seats with harnesses and steering wheel, UV-resistant tinted windows, 8ball gear shifter, and a fuzzy dashboard with a Hawaiian girl bobble-head.*",
-        "inline": true
      },
       {
         "name": "Performance",
-        "value": "*Offers absolutely horrible power steering due to its extremely cumbersome curb weight: 3300lbs. [1497kg]. This can be a potential hazard, especially when achieving speeds of Mach 1. With only 3 MPG, this makes it one of the WORST in fuel efficiency as it burns with a V8 Duramax 9.7L, outputting a 0–60 in 1.7 seconds. Most of its acceleration power is placed on the wheels themselves, where the drivetrain is an all-wheel drive (AWD) Despite its horrible weight, the speed significantly makes that factor up when its top speed is almost equivalent to a Boeing jet: 767mph [1234kph]*",
-        "inline": true
+        "value": "*Offers absolutely horrible power steering due to its extremely cumbersome curb weight: 3300lbs. [1497kg]. This can be a potential hazard, especially when achieving speeds of Mach 1. With only 3 MPG, this makes it one of the WORST in fuel efficiency as it burns with a V8 Duramax 9.7L, outputting a 0–60 in 1.7 seconds. Most of its acceleration power is placed on the wheels themselves, where the drivetrain is an all-wheel drive (AWD) Despite its horrible weight, the speed significantly makes that factor up when its top speed is almost equivalent to a Boeing jet: 767mph [1234kph]*"
       }
     ]
   };
