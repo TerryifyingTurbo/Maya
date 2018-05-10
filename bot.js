689d806b667a3694532b9809de33299a108d8348
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-05-09 21:47:54 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -614,7 +614,7 @@ if(command === `${prefix}dwsaeztia`){
   // "*type* __*more*__ *for more info*"
   message.channel.send(aembed).then(message => {
     setTimeout(() => {
-    await message.channel.send("*type* __*more*__ *for more info*")
+      message.channel.send("*type* __*more*__ *for more info*")
   }, cdseconds * 1000)
     
   if(message.content === "more"){
