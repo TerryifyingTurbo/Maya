c6ed109062ce143c2bc38dfd54a108bd79e5cd9c
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-05-13 16:59:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -245,6 +245,92 @@ if(message.content === 'what is my pfp'){
   // if(!User) return message.channel.send(message.author.displayAvatarURL);
   message.channel.send(message.author.displayAvatarURL)
 }
+
+if(command === `${prefix}leet`){
+  let _inverseReplacementsCached = null;
+const getInverseReplacements = replacements => {
+    if (_inverseReplacementsCached) {
+        return _inverseReplacementsCached;
+    }
+    const inverseReplacements = new Map();
+    Object.keys(replacements)
+        .map(letter => {
+            replacements[letter].forEach(replacement => {
+                inverseReplacements.set(new RegExp(global.utils.quoteRegex(replacement), 'gi'), letter);
+            });
+        });
+
+    _inverseReplacementsCached = inverseReplacements;
+
+    return inverseReplacements;
+};
+
+    const parsedArgs = bot.utils.parseArgs(args, ['e', 't']);
+
+    if (parsedArgs.leftover.length < 1) {
+        throw 'Provide text to be leeted.';
+    }
+
+    let parsed;
+
+    if (parsedArgs.options.e) {
+        const extendedLeetReplacements = {
+            'a': ['4', '@', '/-\\', 'Д'],
+            'b': ['ß'],
+            'c': ['¢', '©'],
+            'e': ['3', '€'],
+            'f': ['ph', 'ƒ'],
+            'g': ['6'],
+            'i': ['1', '!'],
+            'l': ['7'],
+            'n': ['И', 'ท'],
+            'q': ['Ø'],
+            'r': ['®', 'Я'],
+            's': ['5', '$', '§'],
+            't': ['†'],
+            'u': ['|_|', 'µ', 'บ'],
+            'v': ['\\/'],
+            'w': ['\\/\\/', 'VV', 'Ш', 'พ'],
+            'x': ['Ж', '×'],
+            'y': ['¥']
+        };
+
+        const inverseReplacements = getInverseReplacements(extendedLeetReplacements);
+        if (parsedArgs.options.t) {
+            parsed = parsedArgs.leftover.join(' ');
+
+            for (let [replacement, origValue] of inverseReplacements) {
+                parsed = parsed.replace(replacement, origValue);
+            }
+        } else {
+            parsed = parsedArgs.leftover
+                .join(' ')
+                .replace(/[a-z]/gi, str => {
+                    let selection = bot.utils.randomSelection(extendedLeetReplacements[str.toLowerCase()] || [str]);
+                    selection = bot.utils.quoteRegex(selection);
+                    return selection;
+                });
+        }
+    } else {
+        const simpleLeetReplacements = '4BCD3F6H1JKLMN0PQR57';
+        if (parsedArgs.options.t) {
+            parsed = parsedArgs.leftover.join(' ').replace(/[a-z0-9]/g, function (a) {
+                let foundInReplacements = simpleLeetReplacements.indexOf(a);
+                if (foundInReplacements === -1) {
+                    return a;
+                }
+                return String.fromCharCode(97 + foundInReplacements);
+            });
+        } else {
+            parsed = parsedArgs.leftover.join(' ').replace(/[a-z]/g, function f(a) {
+                return simpleLeetReplacements[parseInt(a, 36) - 10] || a.replace(/[a-t]/gi, f);
+            }).toLowerCase();
+        }
+    }
+
+    message.delete();
+    message.channel.send(parsed);
+}
   
 if(command === `${prefix}listemotes`){
   const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
@@ -664,6 +750,25 @@ if(command === `${prefix}dwstravis`){
   "This most certainly comes from his time as a catcher and his stint in prison. Taking from his father's side, Travis and Relonimar both take their father's personality but is not as severe. The two brothers also inherit their father's dominant hand, which is the left hand—often frowned upon when doing tasks that were made for the right dominant hand." + 
   "Travis is the person who wears casual clothing that usually consists of dark jeans, black two-tone converses, unique shirts with psychedelic logos covered by his galaxy hoodie.*", true)
   message.channel.send(tembed);
+}
+
+if(command === `${prefix}dwsroxuhn`){
+  let rembed = new Discord.RichEmbed()
+  .setTitle("Roxuhn")
+  .setColor("#000001")
+  .setFooter("Fantasy Character", "https://i.imgur.com/uOFLi1A.png")
+  .setThumbnail("https://i.imgur.com/lCgNS6v.jpg")
+  .addField("Status", "*Alive*", true)
+  .addField("Height", "*5'5/165 cm/1.6 m*", true)
+  .addField("Weight", "*120 lbs./54 kg*", true)
+  .addField("Gender", "*Female*", true)
+  .addField("Age", "*99 Googolplexian*", true)
+  .addField("Zodiac Sign", "*Unknown*", true)
+  .addField("Description", "*Mischievous demon who uses her powers to manipulate others for personal gain and advantages.*", true)
+  .addField("▬▬▬▬▬▬▬▬▬▬▬▬", "Special Abilities", true)
+  .addField("Hypnosis", "*Gain control of a person, animal, and other entities and fully manipulate the victim to any extent or desire.*");
+  message.channel.send(rembed);
+
 }
 
 if(command === `${prefix}dirtyquote`){
