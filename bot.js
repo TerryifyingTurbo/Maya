13b942d7dff263492829fa2f65e2e15b4154dea3
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-05-16 16:07:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -281,7 +281,7 @@ if(command === `${prefix}fox`){
   let foxembed = new Discord.RichEmbed()
   .setColor("RANDOM")
   .setTitle("Foxy :0")
-  .setImage(body.url);
+  .setImage(body.image);
   message.channel.send(foxembed)
   .catch(error => message.channel.send(`Failed. Something went wrong.**${error}**`));
 }
