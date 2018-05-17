7e80338a914a1135612ef01fac345a23e100e6d2
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-05-16 22:24:00 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -469,7 +469,7 @@ if(command === `${prefix}8ball`){
   }
 
   if(command === `${prefix}pick`){
-    let opt = args.join(',').slice(22);
+    let opt = args.join(",");
     let result = Math.floor((Math.random() * args.length));
     if(args.length < 2) return message.channel.send("I need two options to decide.");
     message.channel.send(`I pick ${opt[result]}`);
