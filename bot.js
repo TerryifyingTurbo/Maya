7578710b606ff4345070ee6c5a854f182608b01c
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-05-19 13:57:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -482,13 +482,34 @@ if(command === `dirtyquote`){
   
 if(command === `8ball`){
     if(!args[2]) return message.channel.send("Actually ask a question?");
-    let replies = ["Yes.", "No.", "Maybe.", "Ask some other time.", "Cannot say now.", "Absolutely.", "Without a doubt.", "For real? No."];
+    let replies = ["It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+    "For real? No",
+    "Eh... maybe"];
 
     let result = Math.floor((Math.random() * replies.length))
     let question = args.slice(0).join(" ");
 
     let ballembed = new Discord.RichEmbed()
     .setAuthor(message.member.displayName + " asked—")
+    .setThumbnail("https://findicons.com/files/icons/1700/2d/512/8_ball.png")
     .setColor("RANDOM")
     .addField("Question :8ball:", question)
     .addField("Answer", replies[result]);
