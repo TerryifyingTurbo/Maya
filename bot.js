c794d7fb703bdcc2fdb0e8b2996e1acc14bd5253
TerryifyingTurbo <37434832+TerryifyingTurbo@users.noreply.github.com>
2018-06-11 14:55:51 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -334,8 +334,9 @@ if(command === `mstrains` && (args[0] == "search")){
 
   let descAPI = `http://strainapi.evanbusse.com/qLZXyPT/strains/data/desc/${encodeURIComponent(strainID)}`;
   let effectsAPI = `http://strainapi.evanbusse.com/qLZXyPT/strains/data/effects/${encodeURIComponent(strainID)}`;
-  await snekfetch.get(descAPI).then(r =>{
-    snekfetch.get(effectsAPI)
+  
+  await snekfetch.get(effectsAPI).then(r =>{
+  snekfetch.get(descAPI)
     let body = r.body
     if(!body) return message.channel.send("NOT FOUND");
     //console.log(body)
